b9121b52831473c621b871450b1350903a835f36
Travis Exline <t.exline22@gmail.com>
2019-05-17 16:21:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS << brand unless brand.include?(brand)
   end
 
   def cobble
     self.condition = "new"
-    BRANDS << @brand
     puts "Your shoe is as good as new!"
   end
 
